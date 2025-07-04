4d89135a39e58cfc7c13d35f1006813777fbdeeb
SamarthGowdaMS <samarthgowdams2007@gmail.com>
2025-07-04 12:44:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function submit(){
+var fname=document.getElementById('fname')
+var lname=document.getElementById('lname')
+var fullname=fname.value+" "+lname.value
+console.log(fullname)
+var email=document.getElementById('email')
+console.log(email.value)
+var phno=document.getElementById('phno')
+console.log(phno.value)
+var pword=document.getElementById('pword')
+console.log(pword.value)
+}
